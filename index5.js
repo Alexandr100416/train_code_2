776b4c3702ec62b28b1c5462a8a1b5dba53b9814
unknown <sashamedoliz@gmail.com>
2021-01-05 23:57:41 +0200

    added task number 6

---
+++ index5.js
@@ -0,0 +1,32 @@
+"use strict"
+
+const numbers = [];
+let total = 0;
+let input;
+let update;
+const findNumbers = function(){
+    do {
+        input = prompt("Введите число");
+        update =  Number(input);
+        if(!isNaN(update)){
+            numbers.push(update);
+        }if (isNaN(update)){
+    alert("Ошибка введение даных")
+        }
+    } while (
+        input !== null&&!isNaN(update)
+    );
+}
+findNumbers();
+
+const sum = function (arr) {
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total;
+}
+
+
+console.log(sum(numbers));
+
+
